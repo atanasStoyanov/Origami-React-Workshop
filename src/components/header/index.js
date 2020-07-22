e25519dabd9f5c10d73368abd404102d73f02eb1
atanasStoyanov <atanass@dxc.com>
2020-07-23 00:35:55 +0300

    feat: implement footer component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import styles from './index.module.css';
-import Link from '../link'
+import Link from '../link';
+import logo from '../../images/white-origami-bird.png'
 
 const Header = () => {
     return (
         <header className={styles.navigation}>
-            <ul>
-                <Link href="#" title="Going to 1" type="header"/>
-                <Link href="#" title="Going to 2" type="header"/>
-                <Link href="#" title="Going to 3" type="header"/>
-                <Link href="#" title="Going to 4" type="header"/>
-                <Link href="#" title="Going to 5" type="header"/>
-                <Link href="#" title="Going to 6" type="header"/>
-                <Link href="#" title="Going to 7" type="header"/>
-                <Link href="#" title="Going to 8" type="header"/>
-                <Link href="#" title="Going to 9" type="header"/>
-                <Link href="#" title="Going to 10" type="header" />
-                <Link href="#" title="Going to 11" type="header" />
-            </ul>
+            <img src={logo} className={styles.logo}/>
+            <Link href="#" title="Going to 1" type="header" />
+            <Link href="#" title="Going to 2" type="header" />
+            <Link href="#" title="Going to 3" type="header" />
+            <Link href="#" title="Going to 4" type="header" />
+            <Link href="#" title="Going to 5" type="header" />
+            <Link href="#" title="Going to 6" type="header" />
+            <Link href="#" title="Going to 7" type="header" />
+            <Link href="#" title="Going to 8" type="header" />
+            <Link href="#" title="Going to 9" type="header" />
+            <Link href="#" title="Going to 10" type="header" />
+            <Link href="#" title="Going to 11" type="header" />
         </header>
     )
 }
