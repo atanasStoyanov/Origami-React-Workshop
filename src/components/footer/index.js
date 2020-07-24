aa9d58af4303ce26edf9a5f8cafcdb3241b2cf2a
atanasStoyanov <atanass@dxc.com>
2020-07-24 22:06:06 +0300

    feat: prepare project for next steps

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import Link from '../link';
 import styles from './index.module.css';
 import image from '../../images/blue-origami-bird-flipped.png';
+import getNavigation from '../../utils/navigation';
 
 const Footer = () => {
+    const links = getNavigation();
+
     return (
         <footer>
             <div className={styles.footer}>
-            <Link href="#" title="Going to 1" type="footer" />
-            <Link href="#" title="Going to 2" type="footer" />
-            <Link href="#" title="Going to 3" type="footer" />
-            <Link href="#" title="Going to 4" type="footer" />
-            <Link href="#" title="Going to 5" type="footer" />
-            <Link href="#" title="Going to 6" type="footer" />
-            <Link href="#" title="Going to 7" type="footer" />
-            <Link href="#" title="Going to 8" type="footer" />
-            <Link href="#" title="Going to 9" type="footer" />
-            <Link href="#" title="Going to 10" type="footer" />
-            <Link href="#" title="Going to 11" type="footer" />
+            {
+                links.map(navElement => {
+                    return (
+                        <Link href={navElement.link} title={navElement.title} type="footer" />
+                    )
+                })
+            }
             <img className={styles.logo} src={image}/>
             <p className={styles.university}>Software University 2020</p>
             </div>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import styles from './index.module.css';
 import Link from '../link';
 import logo from '../../images/white-origami-bird.png'
+import getNavigation from '../../utils/navigation'
 
 const Header = () => {
+    const links = getNavigation();
+
     return (
         <header className={styles.navigation}>
-            <img src={logo} className={styles.logo}/>
-            <Link href="#" title="Going to 1" type="header" />
-            <Link href="#" title="Going to 2" type="header" />
-            <Link href="#" title="Going to 3" type="header" />
-            <Link href="#" title="Going to 4" type="header" />
-            <Link href="#" title="Going to 5" type="header" />
-            <Link href="#" title="Going to 6" type="header" />
-            <Link href="#" title="Going to 7" type="header" />
-            <Link href="#" title="Going to 8" type="header" />
-            <Link href="#" title="Going to 9" type="header" />
-            <Link href="#" title="Going to 10" type="header" />
-            <Link href="#" title="Going to 11" type="header" />
+            <img src={logo} className={styles.logo} />
+            {
+                links.map(navElement => {
+                    return (
+                        <Link href={navElement.link} title={navElement.title} type="header" />
+                    )
+                })
+            }
         </header>
     )
 }
